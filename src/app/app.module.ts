680c20ffc76632b2d53d4e2809a29b9d1405d81e
sandeepdj <sandeepdj1988@gmail.com>
2018-03-26 17:41:46 +0530

    local copy

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/login/login.component';
+import { LayoutComponent } from './modules/layout/layout.component';
+import { SidebarComponent } from './modules/sidebar/sidebar.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { HomeComponent } from './modules/home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    LayoutComponent,
+    SidebarComponent,
+    DashboardComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
